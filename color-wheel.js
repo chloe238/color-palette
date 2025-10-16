1f2739d72dd9e64bec5c5145f3698002c0c4eaf3
chloe238 <chloegirl0121@gmail.com>
2025-10-15 21:14:21 -0500

    AAAAAAA

--- color-wheel.js
+++ color-wheel.js
@@ -1,7 +1,6 @@
-
-canvas = document.getElementById('canvas');
-ctx = context.getContext('2d');
-image = ctx.createImageData(200, 200);
-data = image.data;
-ctx.fillStyle = "#FF00FF";
-ctx.fillRect(20, 10, 150, 100);
+// canvas = document.getElementById('canvas');
+// ctx = context.getContext('2d');
+// image = ctx.createImageData(200, 200);
+// data = image.data;
+// ctx.fillStyle = "#FF00FF";
+// ctx.fillRect(20, 10, 150, 100);
